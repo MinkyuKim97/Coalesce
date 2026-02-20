a712074e24f88f4f10d5abebfecec881eb6a41d8
MinkyuKim97 <allkustom97@gmail.com>
2026-02-20 16:08:47 -0500

    Feb 20th_Final code(with secret.h example)

---
+++ 0 Arduino Code/1 Final/Coalesce_ConnectInHand/secrets_example.h
@@ -0,0 +1,22 @@
+// Change this file's name
+// 'secrets_example.h' -> 'secrets.h'
+
+#pragma once
+
+// Change 'WIFI_COUNT' based on the list lenght
+#define WIFI_COUNT 1
+static const char* WIFI_SSIDS[WIFI_COUNT] ={
+
+};
+static const char* WIFI_PASSWORDS[WIFI_COUNT] = {
+
+};
+
+// Fill it up with Firestore databaes key
+#define FIREBASE_API_KEY ""
+#define FIREBASE_EMAIL ""
+#define FIREBASE_PASSWORD ""
+#define FIREBASE_PROJECT_ID ""
+
+// Client ID, change it depends on the user
+#define FIREBASE_CLIENTID "0001"
